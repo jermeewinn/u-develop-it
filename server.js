551004fa01f96057252bb7ef62bc4ca711c03967
Jeremy Huynh <jeahuynh18@gmail.com>
2021-12-08 23:41:28 -0800

    added db.query function to allow us to pull data through node.js

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Default response for any other request (Not Found)
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username
+        user: 'root',
+        //Your MySQL password
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows)
+});
+
+//Default response for any other request (Not Found). 
+//This is known as a catchall route. This needs to be towards the bottom.
 app.use((req, res) => {
     res.status(404).end();
 });
